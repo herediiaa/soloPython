02d86bfdc6d2ebd03b7cc8b6e0703c1087674263
Emmanuel <herediiaa.e@gmail.com>
2023-06-05 01:35:49 -0300

    crando blog

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('blog/', include('blog.urls'), name='blog')
 
 ]
